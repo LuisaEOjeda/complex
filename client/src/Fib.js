934313ea29fbb97d5482c3228a804cddf4830f16
Luisa Ojeda <luisaojeda@gmail.com>
2024-08-20 13:36:35 +0200

    updated credentials in travisCI

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+const axios = require('axios');
 const Fib = () => {
   const [seenIndexes, setSeenIndexes] = useState([]);
   const [values, setValues] = useState({});
