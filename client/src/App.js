b2b3edd1fa67fd4ce831a2cfe3bdd4214e49064d
Luisa Ojeda <luisaojeda@gmail.com>
2024-08-20 12:32:29 +0200

    still error with react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
 
